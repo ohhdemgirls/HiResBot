6053945ba55c1d1fa558a024d909e90ed642b95d
redditbots <bengahtan@Anonymous-Anonymouss-MacBook-Pro.local>
2014-02-11 17:39:25 -0500

    new features

--- HiResBot.py
+++ HiResBot.py
@@ -1,9 +1,6 @@
-import os
-import sys
-import time
-import praw
-from pprint import pprint
+import re, praw, requests, os, glob, sys, time
 from PIL import Image
+from BeautifulSoup import BeautifulSoup
 
 __author__ = 'Ben and Daniel'
 __version__ = '0.0.1'
@@ -11,33 +8,81 @@ __version__ = '0.0.1'
 class RedditBot(object):
 
     def __init__(self, uname, passwd):
-        self.api = praw.Reddit(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0')     # Change 'SadiqBot' to your bot's name
+        # Identify with name of bot
+        self.api = praw.Reddit(user_agent='HiResBot')
+        # Which subreddit to run on
         self.input = raw_input("What subreddit would you like to run on? ")
         self.subreddit = self.api.get_subreddit(self.input)
-
-    def check_higher_res(self, url):
-        im = Image.open(url)
-        dpi = im.info['dpi']
-        return dpi
+        # Some regex
+        self.imgurUrlPattern = re.compile(r'(http://i.imgur.com/(.*))(\?.*)?')
 
     def upload_new(self):
-         #upload_image(self.subreddit, image_path, name=None, header=False)
+         #self.api.upload_image(self.subreddit, image_path, name=None, header=False)
          pass
+
+    def checkHigherRes(self, imageUrl):
+        print imageUrl
 
     def start(self):
         while True:
-            # get links from subreddit we are interested in
+            # Get links from subreddit we are interested in
             for submission in self.subreddit.get_hot(limit=10):
-                check_higher_res(submission.url)
+                # Check for the cases where we will skip a submission:
+                if "imgur.com/" not in submission.url:
+                    continue # skip non-imgur submissions
+                if 'http://imgur.com/a/' in submission.url:
+                    # This is an album submission.
+                    albumId = submission.url[len('http://imgur.com/a/'):]
+                    htmlSource = requests.get(submission.url).text
 
-            #call check_higher_res
-            #call upload_new
+                    soup = BeautifulSoup(htmlSource)
+                    try:
+                        matches = soup.select('.album-view-image-link a')
+                        for match in matches:
+                            imageUrl = match['href']
+                            if '?' in imageUrl:
+                                imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+                            else:
+                                imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+                            self.checkHigherRes('http:' + match['href'])
+                    except Exception as e:
+                        print e.message
+
+                elif 'http://i.imgur.com/' in submission.url:
+                    # The URL is a direct link to the image.
+                    mo = self.imgurUrlPattern.search(submission.url) # using regex here instead of BeautifulSoup because we are pasing a url, not html
+
+                    imgurFilename = mo.group(2)
+                    if '?' in imgurFilename:
+                        # The regex doesn't catch a "?" at the end of the filename, so we remove it here.
+                        imgurFilename = imgurFilename[:imgurFilename.find('?')]
+
+                    self.checkHigherRes(submission.url)
+
+                elif 'http://imgur.com/' in submission.url and 'http://imgur.com/' not in submission.url:
+                    # This is an Imgur page with a single image.
+                    htmlSource = requests.get(submission.url).text # download the image's page
+                    soup = BeautifulSoup(htmlSource)
+                    try:
+                        imageUrl = soup.select('.image div img')[0]['src']
+                        if imageUrl.startswith('//'):
+                            # if no schema is supplied in the url, prepend 'http:' to it
+                            imageUrl = 'http:' + imageUrl
+                        imageId = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('.')]
+
+                        if '?' in imageUrl:
+                            imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+                        else:
+                            imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+
+                        self.checkHigherRes(imageUrl)
+                    except Exception as e:
+                        print e.message
 
             time.sleep(1800)
 
-
 def main():
-    bot = RedditBot('HiResBot', 'crazychickenbaloons') # Change 'user' and 'pass' to your login info
+    bot = RedditBot('usernname', 'passsword') # Change 'user' and 'pass' to your login info
     try:
         bot.start()
     except:
